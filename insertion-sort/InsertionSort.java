f716b747d10a7ee0a6e12a0348fa4ce4b0ccc921
karthik137 <raokarthik83@gmail.com>
2018-02-09 21:12:40 +0530

    sort function and main method created

---
+++ insertion-sort/InsertionSort.java
@@ -0,0 +1,18 @@
+/**
+@author kirito
+ */
+
+public class InsertionSort{
+    
+    
+    public void sort(){
+
+        System.out.println("****************** Insertion Sort ****************************");
+
+    }
+
+    public static void main(String arg[]){
+        InsertionSort sort = new InsertionSort();
+        sort.sort();
+    }
+}
